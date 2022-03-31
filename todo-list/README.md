00093811e4d9658719a9c66654e12018ee7f7d07
EderBorella <ederlopesborella@gmail.com>
2022-03-31 16:06:35 +0100

    To-do list project, JavaScript, HTML and CSS.

---
+++ todo-list/README.md
@@ -0,0 +1,11 @@
+# This is my fourth project.  
+  
+My task here is to make a "To do list" with some features using HTML, CSS and JavaScript.  
+  
+Features:  
+- Add a task.  
+- Save tasks in the localstorage.  
+- Select a task to move up or down in the list.  
+- Clear the task list, from the localstorage as well.  
+- Remove completed tasks.  
+- Remove selected task.  
---
+++ todo-list/script.js
@@ -0,0 +1,65 @@
+const addTask = document.getElementById('criar-tarefa');
+const inputTask = document.getElementById('texto-tarefa');
+const tasks = document.getElementById('lista-tarefas');
+const removeTask = document.getElementById('remover-finalizados');
+const clear = document.getElementById('apaga-tudo');
+const save = document.getElementById('salvar-tarefas');
+const saveTasks = JSON.parse(localStorage.getItem('tarefas'));
+const moveCima = document.getElementById('mover-cima');
+const moveBaixo = document.getElementById('mover-baixo');
+const clearTask = document.getElementById('remover-selecionado');
+const chosenOne = document.getElementsByClassName('selected');
+
+tasks.innerHTML = saveTasks;
+
+addTask.addEventListener('click', () => {
+  const newTask = document.createElement('li');
+  newTask.innerText = inputTask.value;
+  tasks.appendChild(newTask);
+  inputTask.value = '';
+});
+
+tasks.addEventListener('click', (event) => {
+  for (let i = 0; i < tasks.children.length; i += 1) {
+    tasks.children[i].style.background = '';
+    tasks.children[i].classList.remove('selected');
+  }
+
+  event.target.style.background = 'gray';
+  event.target.classList.add('selected');
+});
+
+tasks.addEventListener('dblclick', (event) => {
+  if (event.target.classList.contains('completed')) {
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
+  }
+});
+
+removeTask.addEventListener('click', () => {
+  document.querySelectorAll('.completed').forEach((element) => element.remove());
+});
+
+clear.addEventListener('click', () => {
+  tasks.innerHTML = '';
+});
+
+save.addEventListener('click', () => {
+  localStorage.setItem('tarefas', JSON.stringify(tasks.innerHTML));
+  alert('Tarefas salvas!');
+});
+
+moveCima.addEventListener('click', () => {
+  if (chosenOne.length > 0) {
+    tasks.children[0] !== chosenOne[0] ? tasks.insertBefore(chosenOne[0], chosenOne[0].previousElementSibling) : false;
+  }
+});
+moveBaixo.addEventListener('click', () => {
+  if (chosenOne.length > 0) {
+    tasks.lastChild !== chosenOne[0] ? tasks.insertBefore(chosenOne[0].nextElementSibling, chosenOne[0]) : false;
+  }
+});
+clearTask.addEventListener('click', () => {
+  chosenOne[0].remove();
+});
